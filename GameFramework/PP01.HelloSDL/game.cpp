8174f1ca9c926e83c9d904a3aabb247255b54d6d
min ju cho <20171280@game.hoseo.edu>
2018-11-15 01:59:38 +0900

    PP04.BasicDrawing

--- GameFramework/PP01.HelloSDL/game.cpp
+++ GameFramework/PP01.HelloSDL/game.cpp
@@ -7,12 +7,21 @@ bool Game::init(const char* title, int xpos, int ypos,
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos,
-			width, height, fullscreen);
+			width, height, /*fullscreen*/SDL_WINDOW_SHOWN);
 
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
+			m_bRunning = true;
+
+			SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+			m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+			SDL_FreeSurface(pTempSurface);
+
+			SDL_QueryTexture(m_pTexture, NULL, NULL,
+				&m_sourceRectangle.w, &m_sourceRectangle.h);
 		}
 
 	}
@@ -20,13 +29,65 @@ bool Game::init(const char* title, int xpos, int ypos,
 	{
 		return false;
 	}
-	m_bRunning = true;
+
 	return true;
 }
 
 void Game::render()
 {
+
+	// source => 원본 사각형
+	// destination => 대상 사각형
+
+	//기본 
+
+	/*m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;*/
+
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+
+	//m_sourceRectangle.x = 50;
+	//m_sourceRectangle.y = 50;
+
+	m_sourceRectangle.x = 50;
+	m_sourceRectangle.y = 50;
+
+	m_destinationRectangle.x = 50;
+	m_destinationRectangle.y = 50;
+
+
+
+	m_sourceRectangle.w = 30;
+	m_sourceRectangle.h = 30;
+
+
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
+
+	m_destinationRectangle.w = 100;
+	//추가문제 
+
+	/*m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
+
+	m_sourceRectangle.x = 50;
+	m_sourceRectangle.y = 50;
+
+	m_destinationRectangle.x = 100;
+	m_destinationRectangle.y = 100;
+
+
+	m_sourceRectangle.w = 50;
+	m_sourceRectangle.h = 50;*/
+
+
+
 	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture,
+		NULL, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
 }
 

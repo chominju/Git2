007bf854b72f4e5d563850193596a889de483901
min ju cho <20171280@game.hoseo.edu>
2018-11-15 01:56:48 +0900

    PP03.GameClass

--- GameFramework/PP01.HelloSDL/main.cpp
+++ GameFramework/PP01.HelloSDL/main.cpp
@@ -1,84 +1,22 @@
-#include<SDL.h>
+#include "game.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-
-bool g_bRunning = false;
-
-bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-
-	else
-	{
-		return false;
-	}
-
-	return true;
-
-}
-
-void render()
-{
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-	SDL_RenderClear(g_pRenderer);
-
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 255, 0, 255);
-
-	/*SDL_RenderClear(g_pRenderer);*/
-
-	SDL_RenderPresent(g_pRenderer);
-
-	g_bRunning = false;
-}
-
-void update()
-{
-
-}
-
-void handleEvents()
-{
-
-}
-
-void clean()
-{
-
-}
-
-
+Game* g_game = 0;
 
 int main(int argc, char* argv[])
 {
-	if (init("Game Framework", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_SHOWN))
+	g_game = new Game();
+	g_game->init("Chapter 1", 100, 100, 640, 480, false);
+
+
+	while (g_game->running())
 	{
-		g_bRunning = true;
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
 	}
 
-	else
-	{
-		return 1;
-	}
-
-
-
-	while (g_bRunning)
-	{
-		render();
-	}
-
-	SDL_Delay(10000);
-	SDL_Quit();
+	g_game->clean();
 
 	return 0;
+
 }
